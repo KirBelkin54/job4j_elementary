3ae40da7737372bfe27a72bd9ad14a56d9a2e396
KirBelkin54 <karen.mcdmc@gmail.com>
2020-03-08 12:38:40 +0700

    4.1.0. Операторы сравнения в String.[#249004]

--- src/ru/job4j/condition/DummyBot.java
+++ src/ru/job4j/condition/DummyBot.java
@@ -1,18 +1,3 @@
 package ru.job4j.condition;
 
  class DummyBot {
-     public static void main(String[] args) {
-        ???
-     }
-     public  String answer(final String question) {
-         final String result;
-         if (question.equals("Привет, Бот.")) {
-             result = "Привет, умник.";
-         } else if (question.equals("Пока.")) {
-             result = "До скорой встречи.";
-         } else {
-             result = "Это ставит меня в тупик. Спросите другой вопрос.";
-         }
-         return result;
-     }
- }
