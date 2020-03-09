dc9cfb7a31d8273795889b15834ccb9460e3e7d4
KirBelkin54 <karen.mcdmc@gmail.com>
2020-03-09 20:34:14 +0700

    2. Тесты для идеальный вес[#249089]

--- src/ru/job4j/calculator/Fit.java
+++ src/ru/job4j/calculator/Fit.java
@@ -3,7 +3,7 @@ package ru.job4j.calculator;
 public class Fit {
 
     public static double manWeight(double height) {
-        return (height - 110) * 1.15;
+        return (height - 100) * 1.15;
     }
 
     public static double womanWeight(double height) {
