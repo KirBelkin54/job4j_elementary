d5f515969c63104efd332a592a2eaf8f1f70f8a3
KirBelkin54 <karen.mcdmc@gmail.com>
2020-04-22 16:07:41 +0700

    6.0.4. Заполнить массив степенями чисел.[#249007]

---
+++ src/ru/job4j/loop/Mortgage.java
@@ -0,0 +1,13 @@
+package ru.job4j.loop;
+
+//public class Mortgage {
+ //   public int year(int amount, int salary, double percent) {
+  //      int year = 0;
+   //     int d = 0;
+   //     while (d<0){
+   //       for ( amount + (salary*percent)=d);
+   //         year++;
+    //    }
+     //   return year;
+   // }
+//}
